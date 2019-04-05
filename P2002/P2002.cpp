9c615944653b5f1587f1035d7d8bb381f264ff56
hyq <544608734@qq.com>
2019-04-05 20:36:21 +0800

    2002

---
+++ P2002/P2002.cpp
@@ -0,0 +1,19 @@
+﻿// P2002.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+#include<cmath>
+#include<iomanip>
+using namespace std;
+#define PI 3.1415927
+int main()
+{
+	double r;
+	while (cin>>r)
+	{
+		cout << fixed << setprecision(3) << 4.0 / 3 * PI*pow(r, 3) << endl;
+	}
+}
+
+
