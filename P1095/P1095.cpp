e31fde490020bc6ce3e4be8aec1dd87873b3cb91
hyq <544608734@qq.com>
2019-04-05 19:03:18 +0800

    1095

---
+++ P1095/P1095.cpp
@@ -0,0 +1,15 @@
+﻿// P1095.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b;
+	while (cin>>a>>b)
+	{
+		cout << a + b << endl << endl;
+	}
+}
+
