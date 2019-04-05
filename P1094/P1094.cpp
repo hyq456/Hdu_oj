4f620c9db19f49960df8b2735d92448610b02102
hyq <544608734@qq.com>
2019-04-05 18:55:49 +0800

    1094

---
+++ P1094/P1094.cpp
@@ -0,0 +1,20 @@
+﻿// P1094.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	int n;
+	while (cin >> n) {
+		int sum = 0;
+		int a;
+		for (int i = 0; i < n; i++) {
+			cin >> a;
+			sum += a;
+		}
+		cout << sum << endl;
+	}
+}
