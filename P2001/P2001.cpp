d16ddda9e3c5f31edf0062e9b0d021d8798a17ad
hyq <544608734@qq.com>
2019-04-05 20:16:54 +0800

    2001

---
+++ P2001/P2001.cpp
@@ -0,0 +1,16 @@
+﻿// P2001.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+int main()
+{
+	double x1, y1, x2, y2;
+	double result;
+	while (cin>>x1>>y1>>x2>>y2)
+	{
+		cout << sqrt(abs(pow(x2 - x1, 2) + pow(y2 - y1, 2))) << endl;
+	}
+}
+
