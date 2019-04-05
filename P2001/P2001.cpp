2c68b69a17f4de17f77f550aa9ec778a5500db0d
hyq <544608734@qq.com>
2019-04-05 20:27:53 +0800

    2001

--- P2001/P2001.cpp
+++ P2001/P2001.cpp
@@ -3,6 +3,8 @@
 
 #include "pch.h"
 #include <iostream>
+#include<cmath>
+#include <iomanip>
 using namespace std;
 int main()
 {
@@ -10,7 +12,7 @@ int main()
 	double result;
 	while (cin>>x1>>y1>>x2>>y2)
 	{
-		cout << sqrt(abs(pow(x2 - x1, 2) + pow(y2 - y1, 2))) << endl;
+		cout <<fixed<< setprecision(2)<< sqrt(abs(pow(x2 - x1, 2) + pow(y2 - y1, 2))) << endl;
 	}
 }
 
